366f1fe80afd57d88dd3234d8c5510ec2d1f4f22
tutich <pkoinange02@gmail.com>
2023-11-22 11:45:06 +0300

    react img style

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ function App() {
   return (
     <div className="App">
       <div className="dane">
-        <img src={dan}/>
-        <h4>Daniel Clifford</h4>
-        <h6>Verified Graduate</h6>
+        <div className="header">
+          <div className="imgg">
+            <img src={dan}/>
+          </div>
+          <div className="content">
+            <h4 className='name'>Daniel Clifford</h4>
+            <h6 className='status'>Verified Graduate</h6>
+          </div>
+        </div>
         <p className='intro'>I received a job offer mid-course, and the subjects I
         learned were current, if not more so, in the company I joined.I honestly feel I got every penny’s worth.</p>
-        <p>
+        <p className='review'>
         “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and
         have heard some people who had an amazing experience here. I signed up for the free intro course and
         found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most
@@ -25,12 +31,15 @@ function App() {
       </div>
 
       <div className="jon">
-        <img src={jona}/>
-        <h4>Jonathan Walters</h4>
-        <h6>Verified Graduate</h6>
-        <></>
-        <p>The team was very supportive and kept me motivated “ I started as a
-        total newbie with virtually no coding skills. I now work as a mobile engineer for a big company.
+        <div className="header">
+          <div className="imgg"><img src={jona}/></div>
+          <div className="content">
+            <h4 className='name'>Jonathan Walters</h4>
+            <h6 className='status'>Verified Graduate</h6>
+          </div>
+        </div>
+        <p className='intro'>The team was very supportive and kept me motivated</p>
+        <p className='review'> “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company.
         This was one of the best investments I’ve made in myself. ”</p>
       </div>
 
@@ -38,18 +47,22 @@ function App() {
         <img src={jean}/>
         <h4>Jeanette Harmon</h4>
         <h6>Verified Graduate</h6>
-        <p>An overall wonderful and rewarding experience “ Thank you for the
-        wonderful experience! I now have a job I really enjoy, and make a good living while doing something I
+        <p className='intro'>An overall wonderful and rewarding experience</p>
+        <p> “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I
         love. ”</p>
       </div>
 
       <div class="patii">
-        <img src={pat}/>
-        <h4>Patrick Abrams</h4>
-        <h6>Verified Graduate</h6>
-        <p>Awesome teaching support from TAs who did the bootcamp themselves.
-        Getting guidance from them and learning from their experiences was easy. “ The staff seem genuinely
-        concerned about my progress which I find really refreshing. The program gave me the confidence necessary
+        <div className="header">
+          <div className="imgg"><img src={pat}/></div>
+          <div className="content">
+            <h4>Patrick Abrams</h4>
+            <h6>Verified Graduate</h6>
+          </div>
+        </div>
+        <p className='intro'>Awesome teaching support from TAs who did the bootcamp themselves.
+        Getting guidance from them and learning from their experiences was easy.</p>
+        <p className='review'> “ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary
         to be able to go out in the world and present myself as a capable junior developer. The standard is above
         the rest. You will get the personal attention you need from an incredible community of smart and amazing
         people. ”</p>
@@ -59,8 +72,8 @@ function App() {
         <img src={kira}/>
         <h4>Kira Whittle</h4>
         <h6>Verified Graduate</h6>
-        <p>Such a life-changing experience. Highly recommended! “ Before joining the
-        bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me
+        <p className='intro'>Such a life-changing experience. Highly recommended!</p>
+        <p> “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me
         learn programming step by step. I was encouraged to enroll by a former student of theirs who can only
         say wonderful things about the program. The entire curriculum and staff did not disappoint. They were
         very hands-on and I never had to wait long for assistance. The agile team project, in particular, was
